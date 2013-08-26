6ec39242cb0cdfeed2b44a0a7db7dac6ebc9a80e
Администратор <administrator@Administrators-Mac.local>
2013-08-26 05:08:01 +0300

    Initial Commit

---
+++ Console/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Console
+//
+//  Created by Администратор on 8/26/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

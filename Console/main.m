ae6a0e954122b56db6a28f60160a72c68a2e9ce5
Администратор <administrator@Administrators-Mac.local>
2013-09-08 16:56:04 +0300

    v1

--- Console/main.m
+++ Console/main.m
@@ -5,15 +5,33 @@
 //  Created by Администратор on 8/26/13.
 //  Copyright (c) 2013 Администратор. All rights reserved.
 //
+@interface Logger : NSObject
 
+-(void) sayOuch:(NSTimer *)t;
+
+@end
+
+@implementation Logger
+
+-(void) sayOuch:(NSTimer *)t {
+    NSLog(@"Ouch");
+}
+
+@end
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        Logger *logger = [[Logger  alloc] init];
+        __unused NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval: 2.0
+                                                          target:logger
+                                                        selector:@selector(sayOuch:)
+                                                        userInfo:nil
+                                                         repeats:YES];
+        [[NSRunLoop currentRunLoop] run];
         
-        // insert code here...
         NSLog(@"Hello, World!");
         
     }
